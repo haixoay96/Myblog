fdb96f71d22623719d0721e881e9a34dab44a33d
Linh Đức <Linh c>
2016-09-09 22:27:24 +0700

    fix nonblocking ver 4

--- app.js
+++ app.js
@@ -124,13 +124,11 @@ db.all('INSERT INTO nodejs (title,content) VALUES (?,?);', 'Cơ chế hoạt đ�
 
 */
 
- var fs = require('fs');
- var x = fs.readFileSync('demo.txt', 'utf8');
- console.log(x);
- var  db = new sqlite3.Database('./databases/dulieu.s3db');
- /*db.all('SELECT title,content FROM nodejs', function (err, rows) {
- });*/
- db.all('UPDATE nodejs SET content = (?) WHERE _id =(?);',x,2);
+/*var fs = require('fs');
+var x =  fs.readFileSync('demo.txt', 'utf8');
+console.log(x);
+var  db = new sqlite3.Database('./databases/dulieu.s3db');
+db.all('UPDATE nodejs SET content = (?) WHERE _id =(?);',x,2);*/
 
 
 
