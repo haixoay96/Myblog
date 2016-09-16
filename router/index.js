455edead15c20d3feb222ece1ac794cfa0b4d31b
Linh Đức <Linh c>
2016-09-16 21:06:51 +0700

    devide router

---
+++ router/index.js
@@ -0,0 +1,17 @@
+var router = express.Router();
+router.get('/', function (req, res) {
+    res.render('index', {
+        main:'Blog Developer !',
+        name: 'Developer',
+        link:'/images/avatar.jpg'
+    });
+});
+router.use(express.static('public'));
+router.use(express.static('node_modules/jquery'));
+router.use(express.static('node_modules/bootstrap'));
+router.use('/programming', require('./programming'));
+router.use('/book', require('./book'));
+router.use(function (req,res) {
+    res.redirect('/');
+});
+module.exports = router;
